3e21eae8f99d5960dd99d02c9e4d416ded8bf595
muhammaddanish-e11 <danisch396@gmail.com>
2022-11-09 16:52:21 +0500

    create user response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create!(user_params)
   end
 
+  private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :nick_name, :phone_number, :email)
+    end
 end
