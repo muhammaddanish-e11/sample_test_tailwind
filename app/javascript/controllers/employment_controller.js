6b0be1b42a279b63d8dcc664c2508b0390ab2acc
muhammaddanish-e11 <danisch396@gmail.com>
2022-11-09 18:26:27 +0500

    quick fix

--- app/javascript/controllers/employment_controller.js
+++ app/javascript/controllers/employment_controller.js
@@ -3,7 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="employment"
 export default class extends Controller {
-  static targets = ["dateStarted"]
+  static targets = ["dateStarted", "dateEnded"]
+
   connect() {
+    $("#dateStarted").datepicker()
+    $("#dateEnded").datepicker()
+  }
+
+  validateDate(e) {
+    debugger
   }
 }
