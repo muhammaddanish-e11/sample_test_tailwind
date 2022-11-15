d8830bf815acb9e0f16260798f9f3fdebff00e23
muhammaddanish-e11 <danisch396@gmail.com>
2022-11-15 18:56:04 +0500

    safety validations at backend

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -1,3 +1,4 @@
 class Employment < ApplicationRecord
+  validates :date_started, :employer, :date_ended , presence: true
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :email, :phone_number , presence: true
+  validates :first_name, length: { maximum: 25 }, presence: true
+  validates :last_name,  length: { maximum: 50 }, presence: true
+
   has_many :employments
   accepts_nested_attributes_for :employments
 end
